36b7389a825ea0cf728b06b054c502dc3ba57956
althafvly <althafvly@gmail.com>
2021-04-21 08:46:07 +0000

    Pegasify

--- aosp_sargo.mk
+++ aosp_sargo.mk
@@ -24,6 +24,8 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 
+$(call inherit-product, vendor/pegasus/config/common.mk)
+
 # Enable mainline checking
 # TODO(b/138706293): Enable mainline checking later
 # PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
